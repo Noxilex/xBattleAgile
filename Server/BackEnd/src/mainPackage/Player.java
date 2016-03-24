376670916293867e800d711ea519ac089add3dab
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2016-03-24 10:39:51 +0100

    Add Case[][] map generation

--- Server/BackEnd/src/mainPackage/Player.java
+++ Server/BackEnd/src/mainPackage/Player.java
@@ -1,11 +1,19 @@
 package mainPackage;
 
+import java.util.ArrayList;
+
 public class Player {
 	
 	private String pseudo;
 	private int gameId;
 	private String skinImg;
-	private int nbrCaseOwn;
-//	private ArrayList<>
+	private int nbrCaseOwned;
+	private ArrayList<Case> ownedCases;
+	
+	public Player(String pseudo, int gameId, String skinImg) {
+		this.pseudo = pseudo;
+		this.gameId = gameId;
+		
+	}
 
 }
