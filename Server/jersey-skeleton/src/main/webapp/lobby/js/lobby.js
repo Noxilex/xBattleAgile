3ad9360c8ab66fc4e1a0441cd409940a49b3a35a
lopezb <benjamin-lopez@hotmail.fr>
2016-03-25 15:12:07 +0100

    Lobby rebuild but not done

---
+++ Server/jersey-skeleton/src/main/webapp/lobby/js/lobby.js
@@ -0,0 +1,9 @@
+var players = [];
+
+function addPlayer(){
+	$(".queue_container .queue_line .queue_item .queue_name:eq(2)").text("OK");
+}
+
+$(function(){
+
+})
---
+++ Server/jersey-skeleton/src/main/webapp/lobby/js/scoreboard.js
@@ -0,0 +1,25 @@
+var myJSONObject = {"players": [
+        {"name": "Henri", "score": 10},
+        {"name": "Paul", "score": 20},
+        {"name": "Claude", "score" : 15}
+    ]
+};
+var players = myJSONObject.players;
+var nbLigne = players.length;
+var table = $('#scoreboard');
+table.html('');
+
+// génération des cellules de la table à l'image de l'exemple
+var header = $('<tr></tr>');
+header.append('<th>Nom</th>');
+header.append('<th>Score</th>');
+table.append(header);
+for(i=0; i < nbLigne; i++){
+	var row = $('<tr></tr>');
+	var p = players[i];
+	for (prop in p){
+		var rawData = $('<td>'+p[prop]+'</td>');
+		row.append(rawData);
+	}
+	table.append(row);
+}
