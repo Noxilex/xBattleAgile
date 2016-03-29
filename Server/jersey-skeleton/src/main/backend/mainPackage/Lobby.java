915419ba0f3b623663d2a410892333901dd04596
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2016-03-29 09:11:11 +0200

    Create lobby

---
+++ Server/jersey-skeleton/src/main/backend/mainPackage/Lobby.java
@@ -0,0 +1,23 @@
+package mainPackage;
+
+public class Lobby {
+	
+	private ArrayList<Player> listPlayers = new ArrayList<Player>();
+
+	public Lobby() {}
+
+	public ArrayList<Player> addPlayer(Player p) {
+		listPlayers.add(p);
+		return listPlayers;
+	}
+
+	public ArrayList<Player> getListPlayer() {
+		return listPlayers;
+	}
+
+	public ArrayList<Player> removePlayerFromList(Player p) {
+		listPlayers.remove(p);
+		return listPlayers;
+	}	
+
+}
